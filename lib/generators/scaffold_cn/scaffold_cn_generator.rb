8edbaf81ffd32c8ec27e1be515b33a2c45362b64
homeway <homeway.xue@gmail.com>
2013-12-03 23:55:35 +0800

    patch for param

--- lib/generators/scaffold_cn/scaffold_cn_generator.rb
+++ lib/generators/scaffold_cn/scaffold_cn_generator.rb
@@ -2,20 +2,19 @@
 class Scaffold_cnGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
 
-  def generate_scaffold_cn type
-    puts "type: #{type}"
+  def generate_scaffold_cn
     templates = [
         'lib/templates/erb/scaffold/_form.html.erb',
         'lib/templates/erb/scaffold/edit.html.erb',
         'lib/templates/erb/scaffold/index.html.erb',
         'lib/templates/erb/scaffold/new.html.erb',
         'lib/templates/erb/scaffold/show.html.erb',
-        'config/locales/defaults/zh_CN.yml',
-        'config/locales/models/zh_CN.yml',
-        'config/locales/views/zh_CN.yml',
-        'config/locales/devise/zh_CN.yml']
+        'config/locales/defaults.zh_CN.yml',
+        'config/locales/models.zh_CN.yml',
+        'config/locales/views.zh_CN.yml',
+        'config/locales/devise.zh_CN.yml']
     templates.each do |path|
-      template path, path
+      copy_file path, path
     end
   end  
 end
--- lib/scaffold_cn/version.rb
+++ lib/scaffold_cn/version.rb
@@ -1,3 +1,3 @@
 module ScaffoldCn
-  VERSION = "0.1.3"
+  VERSION = "0.1.5"
 end
